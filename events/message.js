2a48c5838042c2973cf171acdbaa1c16cf6d18a0
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-02-11 17:10:55 +0100

    Added Notification message for new issues & suggestions in #discussions

--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
-const {suggestionChannels} = require('../config.json');
+var {suggestionChannels} = require('../config.json'),
+  Discord = require('discord.js')
 
 module.exports = async message => {
   //* Bot == STOP
@@ -8,5 +9,21 @@ module.exports = async message => {
   if(suggestionChannels.includes(message.channel.id)) {
     message.react("528944776867741716")
     .then(() => message.react("528948590739980289"))
+  }
+
+  //* Send Notification of Suggestion/Issue in #general
+  if(suggestionChannels.includes(message.channel.id)
+  || message.channel.id == "528935415982587904") {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(message.member.displayName, message.author.avatarURL)
+    .setURL(`https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`)
+    .setColor("#00FF8C")
+    .setDescription(message.cleanContent.length < 60 ? message.cleanContent : message.cleanContent.slice(0, 60) + "...")
+    .setTitle(`:incoming_envelope: Jump to ${suggestionChannels.includes(message.channel.id) ? "suggestion" : "issue"}`)
+    .setFooter(`Posted in #${message.channel.name}`)
+    
+
+    message.guild.channels.get("528932649394241536")
+    .send(embed)
   }
 }
